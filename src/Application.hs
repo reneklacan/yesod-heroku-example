0470c673dbbc9a9b176c7609e4fb3f93629a7299
Rene Klacan <rene.klacan@gmail.com>
2017-10-08 01:06:31 +0200

    Initial commit

---
+++ src/Application.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE ViewPatterns         #-}
+
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Application where
+
+import Foundation
+import Yesod.Core
+
+import Add
+import Home
+
+mkYesodDispatch "App" resourcesApp
