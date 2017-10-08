f62475a813b1c8dec76b77952d601ebed47db1ba
Rene Klacan <rene.klacan@gmail.com>
2017-10-08 02:12:20 +0200

    Allow to specify any port via env var

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 import Application () -- for YesodDispatch instance
 import Foundation
 import Yesod.Core
+import System.Environment
 
 main :: IO ()
-main = warp 5000 App
+main = do
+  maybePort <- lookupEnv "PORT"
+  let port =
+        case maybePort of
+          Nothing -> 5000
+          Just portStr -> read portStr :: Int
+  warp port App
