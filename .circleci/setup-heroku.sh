dd9cc7c3476f5ca8e8bf10effff9ddb3a5bc4204
Rene Klacan <rene.klacan@gmail.com>
2017-10-08 17:38:44 +0200

    Refactor CI config

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git remote add heroku https://git.heroku.com/yesod-heroku-test.git
+git remote add heroku https://git.heroku.com/$HEROKU_PROJECT.git
 wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
 sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
